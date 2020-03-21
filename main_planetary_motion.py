6e57a7f37de1325f147eaafaa05d9b52cd8cc044
Daniel <djisenberg2015@gmail.com>
2020-03-21 12:15:11 -0400

    Now have working model of the sun and earth.

--- main_planetary_motion.py
+++ main_planetary_motion.py
@@ -3,15 +3,15 @@ import numpy as np
 from vpython import *
 
 title = "test scene"
-L = 5.2
-massSun = 2e30
-scene = canvas(title=title, width=640, height=480, range=2*L, forward=vector(-1, -1, -1))
+scene = canvas(title=title, width=640, height=480, forward=vector(-1, -1, -1))
 
-year = 3.2e7         #convert time to years if necessary
-AU = 1.5e11          #convert  meters to Astronomical Units if necessary
+year = 3.2e7         # convert time to years if necessary
+AU = 1.5e11          # convert  meters to Astronomical Units if necessary
 GMsun = 4*pi**2
 
-#table of masses in units of solar mass
+timeStep = 0.01
+
+# table of masses in units of solar mass
 SolarMass = 2e30
 massSun = 2.0e30 / SolarMass
 massMercury = 2.4e23 / SolarMass
@@ -24,7 +24,7 @@ massUranus = 8.8e25 / SolarMass
 massNeptune = 1.03e26 / SolarMass
 massPluto = 6.0e24 / SolarMass
 
-#table of orbital radii in AU
+# table of orbital radii in AU
 orbital_radius_mercury = 0.39
 orbital_radius_venus = 0.72
 orbital_radius_earth = 1.001
@@ -35,9 +35,9 @@ orbital_radius_uranus = 19.19
 orbital_radius_neptune = 30.06
 orbital_radius_pluto = 39.53
 
-#table of planetary radii (ADD MORE HERE)
+# table of planetary radii (ADD MORE HERE)
 radius_sun = 2e9/AU
 radius_earth = 6.4e6/AU
 radius_jupiter = 11.2*6.4e6/AU
 
-twoBodyProblem.run(orbital_radius_earth, 1, 6.0e24/massSun)
+twoBodyProblem.run(orbital_radius_earth, 1, timeStep)
--- twoBodyProblem.py
+++ twoBodyProblem.py
@@ -1,65 +1,41 @@
 from vpython import *
 import numpy as np
 
+'''
+UNITS
+    1 AU = 1.5 x 10^11  meters
+    1 year = 3.2 x 10^7 seconds
+'''
 
-def run(orbitRadius, orbitPeriod, planetMass):
-    timeStep = orbitPeriod/1000
-    solarGravitationConstant = 4 * (np.pi ** 2)
-    currentTime = 0.0
-    L = 5.2
-    counter = 0
-    AU = 1.5e11
 
-    planetSize = 0.2
+def run(orbitRadius, orbitPeriod, timeStep):
+    axisLength = 5
+
+    earthSize = 0.2
     sunSize = 0.5
-    trailRadius = planetSize/2
+    trailRadius = 0
+    maxTrailLength = np.inf
 
     circularSpeed = (2 * np.pi * orbitRadius) / orbitPeriod
     planetPosition = vector(orbitRadius, 0, 0)
-    planetVelocity = vector(0, circularSpeed, 0)
 
-    xAxis = curve(pos=[vector(0, 0, 0), vector(L, 0, 0)], color=color.red)
-    yAxis = curve(pos=[vector(0, 0, 0), vector(0, L, 0)], color=color.green)
-    zAxis = curve(pos=[vector(0, 0, 0), vector(0, 0, L)], color=color.blue)
+    xAxis = curve(pos=[vector(0, 0, 0), vector(axisLength, 0, 0)], color=color.red)
+    yAxis = curve(pos=[vector(0, 0, 0), vector(0, axisLength, 0)], color=color.green)
+    zAxis = curve(pos=[vector(0, 0, 0), vector(0, 0, axisLength)], color=color.blue)
 
     sun = sphere(pos=vector(0, 0, 0), radius=sunSize, color=color.yellow)
-    planet = sphere(pos=vector(planetPosition.x, planetPosition.y, planetPosition.z), radius=planetSize, color=color.cyan)
-    planet.trail = curve(pos=[planet.pos], color=color.cyan, radius=trailRadius)
+    earth = sphere(pos=vector(planetPosition.x, planetPosition.y, planetPosition.z), radius=earthSize, color=color.cyan)
+    earth.trail = curve(pos=[earth.pos], color=color.cyan, radius=trailRadius, interval=30, retain=maxTrailLength)
 
-    '''
-    Remember: 
-    Force = mass * acceleration
-    Momentum = mass * velocity
-    
-    Acceleration = 1st derivative of velocity = 2nd derivative of position
-    Velocity = 1st derivative of position = integral of acceleration
-    Position = integral of velocity
-    
-    Newtons law of gravitation:
-        F = (gravitationalConstant * sunMass * earthMass) / r ** 2
-        where F is the magnitude of the force between the sun and the earth, r is the distance between the sun and earth
-    
-    x component of gravitational force:
-        -(gravitationalConstant * sunMass * earthMass * earthPositionX)/r ** 3
-        (y is the same but swapped)
-    
-    
-    UNITS
-        1 AU = 1.5 x 10^11  meters
-        1 year = 3.2 x 10^7 seconds
-         
-    
-    '''
+    earthPosition = vector(1, 0, 0)
+    earthVelocity = vector(0, circularSpeed, 0)
 
-    while 1: # the problem is I am treating the acceleration as a scalar when it is in fact a vector, even if it is only in one or two of the components, treating it as a scalar implies that it is equal in ALL components.
-        planetAcceleration = ((solarGravitationConstant * planetMass) / (orbitRadius ** 2) / planetMass)
-        planetVelocity.x = planetVelocity.x + (planetAcceleration * timeStep)
-        planetVelocity.y = planetVelocity.y + (planetAcceleration * timeStep)
-        #planetVelocity.z = planetVelocity.z + (planetAcceleration * timeStep)
-        planetPosition = planetPosition + (planetVelocity * timeStep)
-        currentTime = currentTime + timeStep
-        counter = counter + 1
-        planet.pos = planetPosition
-
-        rate(60)  # note to self: what does rate(x) do exactly? does it make the program run at most x frames/iterations/steps per second? does this apply only to the frame rate and not to how many steps are actually being applied? (3-21-2020, remove when question has been answered)
-        planet.trail.append(planet.pos)
+    while 1:
+        distanceEarthSun = np.sqrt((earthPosition.x ** 2 + earthPosition.y ** 2))
+        earthVelocity.x = earthVelocity.x - ((4 * np.pi ** 2 * earthPosition.x) / distanceEarthSun ** 3) * timeStep
+        earthVelocity.y = earthVelocity.y - ((4 * np.pi ** 2 * earthPosition.y) / distanceEarthSun ** 3) * timeStep
+        earthPosition.x = earthPosition.x + (earthVelocity.x * timeStep)
+        earthPosition.y = earthPosition.y + (earthVelocity.y * timeStep)
+        rate(60)
+        earth.pos = earthPosition
+        earth.trail.append(earth.pos)
